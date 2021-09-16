b2328f658a1d2d28912f9e59f341efd0beab4e0b
TobiasKruzhor <278259147@qq.com>
2021-09-16 18:24:08 +0800

    20210915 commit

--- sections 8/errhanding/filelistingserver/errwrapper_test.go
+++ sections 8/errhanding/filelistingserver/errwrapper_test.go
@@ -1,10 +1,45 @@
 package main
 
 import (
+	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
+func errPanic(writer http.ResponseWriter, request *http.Request) error {
+	panic(123)
+
+}
+
+
+func errUserError(writer http.ResponseWriter, request *http.Request) error {
+
+}
+
 func TestErrWrapper(t *testing.T) {
-	httptest.NewRecorder()
+	tests := []struct {
+		h       appHandler
+		code    int
+		message string
+	}{
+		{errPanic, 500, "Internal Server Error"},
+	}
+
+	for _, tt := range tests {
+		f := errWrapper(tt.h)
+		response := httptest.NewRecorder()
+		request := httptest.NewRequest(
+			http.MethodGet,
+			"http://www.imooc.con", nil)
+		f(response, request)
+
+		b, _ := ioutil.ReadAll(response.Body)
+		body := strings.Trim(string(b), "\n")
+		if response.Code != tt.code || body != tt.message {
+			t.Errorf("expect (%d, %s); got (%d, %s)", tt.code, tt.message, response.Code, body)
+		}
+	}
+	// httptest.NewRecorder()
 }
