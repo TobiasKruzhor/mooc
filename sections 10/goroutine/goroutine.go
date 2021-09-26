436059a208cc37678c7d426602c11b252fd1622f
TobiasKruzhor <278259147@qq.com>
2021-09-26 18:11:49 +0800

    20210925 commit

--- sections 10/goroutine/goroutine.go
+++ sections 10/goroutine/goroutine.go
@@ -6,12 +6,15 @@ import (
 )
 
 func main() {
-	for i := 0; i < 1000; i++ {
+	var a [10]int
+	for i := 0; i < 10; i++ {
 		go func(i int) {
 			for {
-				fmt.Printf("Hello form goroutine %d\n", i)
+				// fmt.Printf("Hello form goroutine %d\n", i)
+				a[i]++
 			}
 		}(i)
 	}
 	time.Sleep(time.Microsecond)
+	fmt.Println(a)
 }
