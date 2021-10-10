2c0800a00923b9756a6fc3f982e212c33a688095
TobiasKruzhor <278259147@qq.com>
2021-10-10 18:29:54 +0800

    20211010 commit

---
+++ sections 10/channel/channel.go
@@ -0,0 +1,10 @@
+package main
+
+func chanDemo() {
+	// var c chan int // c == nil
+
+}
+
+func main() {
+	chanDemo()
+}
--- sections 10/goroutine/goroutine.go
+++ sections 10/goroutine/goroutine.go
@@ -7,11 +7,15 @@ import (
 
 func main() {
 	var a [10]int
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 1000; i++ {
 		go func(i int) {
 			for {
-				// fmt.Printf("Hello form goroutine %d\n", i)
-				a[i]++
+				fmt.Printf("Hello form goroutine %d\n", i)
+
+				/*
+					a[i]++
+					runtime.Gosched()
+				*/
 			}
 		}(i)
 	}
